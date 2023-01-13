cf33175e768bf0120bc2bbd55df96278c7dcd635
franc51 <101558212+franc51@users.noreply.github.com>
2023-01-13 11:59:10 +0200

    added gsap for welcome

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-gsap.from(".navbar", { duration: 2, y: "-100%" });
+gsap.from(".navbar", { duration: 1.2, y: "-100%" });
 gsap.from(".header", { duration: 1, x: "100%" });
+gsap.from(".titlu", { duration: 1.5, x: "200%" });
